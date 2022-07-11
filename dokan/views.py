21ec004eb41f57cd9dc5f3560b27d73b415f6199
mahmud002 <87866290+mahmud002@users.noreply.github.com>
2022-07-11 21:29:43 +0100

    second

--- dokan/views.py
+++ dokan/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import pymongo
 # Create your views here.
 def index(request):
-    return render(request,'home.html')
+    
+    client = pymongo.MongoClient("mongodb+srv://hasan:mahmud01@cluster0.dx63w.mongodb.net/?retryWrites=true&w=majority")
+    db = client.test
+    print(db)
+    mydb = client["mobile"]
+    mycol1 = mydb["mobile_details"]
+    a=mycol1.find()
+    return render(request,'home.html',{'data':a})
